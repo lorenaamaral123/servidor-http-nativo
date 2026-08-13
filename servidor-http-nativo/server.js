d52741e9c4381724c762a416aac6bc3220af1365
Lorena <lorena.amaral@estudante.ifms.edu.br>
2026-08-13 17:13:52 +0000

    .

--- servidor-http-nativo/server.js
+++ servidor-http-nativo/server.js
@@ -1,15 +1,18 @@
-const http = require('node:http');
+const http = require('node:http')
+
+const porta = 3000
 
 const server = http.createServer();
 
-const port = 3000;
-
 server.on('request', (req, res) => {
-    console.log('requisição recebida! Método ${req.method} e Rota: ${req.url}');
-    res.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});
-    res.end("Servidor HTTP nativo Funcionando!");
+    console.log(`Requisição recebida! ${req.method} ${req.url}`);
+
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    res.end("Servidor Funcionando!");    
 });
 
-server.listen(port, () => {
-    console.log('Servidor escutantdo na porta ${port}');
+server.listen(porta, ()=> {
+    console.log(`Servidor ouvindo na porta ${porta}`)
 });
+
